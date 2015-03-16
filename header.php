b1e1484087132ae10221017d3803e342d99570b4
Rami James <ramijames@gmail.com>
2015-03-16 14:29:30 +0200

    Favicon set

--- header.php
+++ header.php
@@ -19,6 +19,23 @@
   <meta name="apple-mobile-web-app-status-bar-style" content="white">
   <meta name="application-name" content="&nbsp;">
   <meta name="google-site-verification" content="tIL3l7s_hfjfJXHYjt40xS9YOtSHNmJaAdjTNrqjOFs" />
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo( 'template_directory' ); ?>/images/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo( 'template_directory' ); ?>/images/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo( 'template_directory' ); ?>/images/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo( 'template_directory' ); ?>/images/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo( 'template_directory' ); ?>/images/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo( 'template_directory' ); ?>/images/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo( 'template_directory' ); ?>/images/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo( 'template_directory' ); ?>/images/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo( 'template_directory' ); ?>/images/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="<?php bloginfo( 'template_directory' ); ?>/images/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo( 'template_directory' ); ?>/images/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="<?php bloginfo( 'template_directory' ); ?>/images/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo( 'template_directory' ); ?>/images/favicon-16x16.png">
+  <link rel="manifest" href="<?php bloginfo( 'template_directory' ); ?>/images/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="<?php bloginfo( 'template_directory' ); ?>/images/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
 
   <link rel="profile" href="http://gmpg.org/xfn/11" />
 
