c332b56cbfa6c3ef81581bc48d3e10773912f329
Rami James <ramijames@gmail.com>
2015-03-16 13:46:31 +0200

    Portfolio update 4

--- footer.php
+++ footer.php
@@ -65,6 +65,17 @@
 <script src="<?php bloginfo( 'template_directory' ); ?>/js/classie.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php bloginfo( 'template_directory' ); ?>/js/app.js" type="text/javascript" charset="utf-8"></script>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60798135-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <?php wp_footer(); ?>
 </body>
 </html>
