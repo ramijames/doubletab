cf90438535dbf6a95b982e6290a86288142ce65b
Rami James <ramijames@gmail.com>
2015-03-16 09:36:34 +0200

    Portfolio update

--- post-formats/format.php
+++ post-formats/format.php
@@ -1,8 +1,8 @@
 
-            <div id="post-<?php the_ID(); ?>" <?php post_class('row'); ?> role="article" itemscope itemprop="blogPost" itemtype="http://schema.org/BlogPosting"  style="border-bottom:2px solid <?php the_field('bg_color'); ?>">
+            <div id="post-<?php the_ID(); ?>" <?php post_class('row'); ?> role="article" itemscope itemprop="blogPost" itemtype="http://schema.org/BlogPosting" style="background-image:url(<?php the_field('bg_background'); ?>)">
               
-                <div class="row">
-                  <div href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>" id="post-<?php the_ID(); ?>" <?php post_class( 'eight columns dailyitem inner-post-image' ); ?> role="article" style="background-image:url(<?php the_field('bg_background'); ?>)">  
+                <div class="three columns">
+                  <div href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>" id="post-<?php the_ID(); ?>" <?php post_class( 'eight columns dailyitem inner-post-image' ); ?> role="article">  
                     <div class="inner-logo-box" style="background-image:url('<?php the_field('logo'); ?>')">
                       <span class="inner-logo-name"><?php the_title(); ?></span>
                     </div>
@@ -10,8 +10,9 @@
                   </div>
                 </div>
 
-                <div class="row bg-white">
-                  <section class="eight columns padding60" itemprop="articleBody">
+                <div class="five columns bg-white">
+                  <section style="background-image:url(<?php the_field('bg_background'); ?>)"></section>
+                  <section class="padding60" itemprop="articleBody">
                     <?php
                       // the content (pretty self explanatory huh)
                       the_content();
