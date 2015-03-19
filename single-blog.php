86cf1660a59073e1acd12e6b617e1a7c1871eaca
Rami James <ramijames@gmail.com>
2015-03-19 08:24:27 +0200

    Portfolio update

--- single-blog.php
+++ single-blog.php
@@ -17,20 +17,20 @@ Single Post Template: Single Blog
 			<div class="row entry-content">
 
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
+							
+							<section class="blog-post-image eight columns padding100 centered" style="background-image:url(<?php the_field('bg_background'); ?>)">
+								<h1 class="white"><?php the_title(); ?></h1>
+								<h5 class="white">
+								<?php printf( __( 'Posted %1$s by %2$s', 'bonestheme' ),
+                     /* the time the post was published */
+                     '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+                      /* the author of the post */
+                      '<span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+                  ); ?>
+                </h5>
+                <?php echo sharing_display(); ?>
+							</section>
 							<article class="four columns offset-by-two padding100">
-								<div id="inner-content">
-									<h1><?php the_title(); ?></h1>
-									<h2>
-									<?php printf( __( 'Posted %1$s by %2$s', 'bonestheme' ),
-	                     /* the time the post was published */
-	                     '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-	                      /* the author of the post */
-	                      '<span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-	                  ); ?>
-	                </h2>
-	                <hr>
-								</div>
 								<div id="inner-content">
 									<section itemprop="articleBody">
 										<?php
